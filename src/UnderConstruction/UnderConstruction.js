0860daafeadb2cc2d1e6022eb0a0edf47af05707
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-28 22:46:04 +0000

    under construction component

---
+++ src/UnderConstruction/UnderConstruction.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UnderConstruction.css';
+
+const UnderConstruction = () => {
+    return (
+        <div className="UnderConstruction" />
+    );
+};
+
+export default UnderConstruction;
