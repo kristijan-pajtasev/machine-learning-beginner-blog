a745986f1c391bacb6f98c7240d144c2529e1756
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-28 22:31:17 +0000

    renamed component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import MLBlog from './MLBlog';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<MLBlog />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/MLBlog.js
+++ src/MLBlog.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './App.css';
+import './MLBlog.css';
 
-function App() {
+function MLBlog() {
   return (
     <div className="App">
       Under construction
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+export default MLBlog;
