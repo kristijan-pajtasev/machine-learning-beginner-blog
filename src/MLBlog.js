f99f21dafd466684f8ad74ed0180d5f5526838d4
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-28 22:33:48 +0000

    init header component

--- src/MLBlog.js
+++ src/MLBlog.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './MLBlog.css';
+import Header from "./Header";
 
 function MLBlog() {
   return (
     <div className="App">
+        <Header/>
       Under construction
     </div>
   );
