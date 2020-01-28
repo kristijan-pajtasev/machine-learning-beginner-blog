92f6b44e4fd60f987e532820843e84c6f0c1a821
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-28 22:57:04 +0000

    init posts endpoint

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = 8003;
 
 app.use(express.static('build'));
 
+app.get('/posts', (req, res) => {
+    res.json({})
+});
+
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`)
 });
