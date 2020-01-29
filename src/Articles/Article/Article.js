8df2a9acc5c00627486fe71a8b0959cd2345ab8b
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-29 10:23:55 +0000

    renderpost html

--- src/Articles/Article/Article.js
+++ src/Articles/Article/Article.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Article.css';
 
-const Article =({title, created, post}) => {
+const Article =({title, created, post, id}) => {
     const getCreatedDate = (date) => date.substr(0,10);
+    const replaceImages = text => text;
+    const parseInlineElements = text => replaceImages(text);
+    const getParsedArticle = text => text.split(/\n+/gi).map((paragraph, index) => <p key={`article_${id}_paragraph_${index}`}>{parseInlineElements(paragraph)}</p>)
 
     return (
         <div className='Article'>
             <h3 className='Article__title'>{title}</h3>
 
-            <div className='Article__post'>
-                {post}
+            <div className='Article__post' dangerouslySetInnerHTML={{__html:post}}>
             </div>
 
-            <div className='Article__created'>
+            <div className='Article__created' >
                 {getCreatedDate(created)}
+
             </div>
 
         </div>
