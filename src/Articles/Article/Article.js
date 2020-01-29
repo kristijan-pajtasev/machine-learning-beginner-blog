f5069076d0025550be93124ea40df0749e5eae76
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-29 01:26:13 +0000

    some styling

--- src/Articles/Article/Article.js
+++ src/Articles/Article/Article.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './Article.css';
 
 const Article =({title, created, post}) => {
+    const getCreatedDate = (date) => date.substr(0,10);
+
     return (
         <div className='Article'>
-            <div className='Article_title'>{title}</div>
+            <h3 className='Article__title'>{title}</h3>
 
             <div className='Article__post'>
                 {post}
             </div>
 
             <div className='Article__created'>
-                {created}
+                {getCreatedDate(created)}
             </div>
 
         </div>
