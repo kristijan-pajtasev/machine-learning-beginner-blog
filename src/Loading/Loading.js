bd0a40c7a98c51ab20e945c36e6754663f47a5a6
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-29 18:14:56 +0000

    init loading component

---
+++ src/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+    return (
+        <div className="Loading" >
+            Under construction
+        </div>
+    );
+};
+
+export default Loading;
