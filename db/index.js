32844002051620b75ff5fb629a201737fad76c6b
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-30 01:45:37 +0000

    get post by id

--- db/index.js
+++ db/index.js
@@ -9,5 +9,16 @@ module.exports = {
             success(res.rows);
             client.end()
         });
+    },
+    getPostById: (id, success, fail) => {
+        const client = new Client(db);
+        client.connect();
+        client.query('SELECT id, post, description, created, title from posts where id=$1',[id], (err, res) => {
+            if(res.rows.length === 1)
+                success(res.rows[0]);
+            else
+                fail();
+            client.end()
+        });
     }
 };
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const {getPosts} = require("./db");
+const {getPosts, getPostById} = require("./db");
 const app = express();
 const port = 8003;
 
 app.use(express.static('build'));
+
+app.get('/posts/:id', (req, res) => {
+    getPostById(
+        req.params.id,
+        (data) => {
+            res.json(data)
+        }, () => {
+            res.status(404).send("Post not found!!")
+        }
+    );
+});
 
 app.get('/posts', (req, res) => {
     getPosts(
