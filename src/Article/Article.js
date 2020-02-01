1950331990d1c4f491b299eee958a55d65366d6c
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-02-01 23:07:05 +0000

    loading icon for article

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import './Article.css';
+import Loading from "../Loading";
 
-const Article =({title, created, post, id}) => {
+const Article =({id}) => {
     const getCreatedDate = (date) => date.substr(0,10);
 
-    if(!id) return <div>In progress</div>
+    if(id) return <Loading />
 
-    return (
-        <div className='Article'>
-            <h3 className='Article__title'>{title}</h3>
-
-            <div className='Article__post' dangerouslySetInnerHTML={{__html:post}}>
-            </div>
-
-            <div className='Article__created' >
-                {getCreatedDate(created)}
-
-            </div>
-
-        </div>
-    )
+    // return (
+    //     <div className='Article'>
+    //         <h3 className='Article__title'>{title}</h3>
+    //
+    //         <div className='Article__post' dangerouslySetInnerHTML={{__html:post}}>
+    //         </div>
+    //
+    //         <div className='Article__created' >
+    //             {getCreatedDate(created)}
+    //
+    //         </div>
+    //
+    //     </div>
+    // )
 };
 
 export default Article;
