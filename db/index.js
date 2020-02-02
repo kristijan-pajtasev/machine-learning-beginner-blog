cf4496f6cd18b1d7fa329c4205d702aec8dce4ca
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-02-02 03:09:56 +0000

    removed post content from posts list

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ module.exports = {
     getPosts: (success, fail) => {
         const client = new Client(db);
         client.connect();
-        client.query('SELECT id, post, description, created, title from posts', (err, res) => {
+        client.query('SELECT id, description, created, title from posts', (err, res) => {
             success(res.rows);
             client.end()
         });
